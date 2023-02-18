e63b557cbf0b72cbf23ca11e48914075bd64ce64
Kirill <kirill.baikin@gmail.com>
2023-02-18 21:12:08 +0300

    feat(gigachads-front): tests

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
             </Head>
             <main>
                 <h1>Hello, world!</h1>
-                <div>test</div>
+                <div>test 1</div>
             </main>
         </>
     )
