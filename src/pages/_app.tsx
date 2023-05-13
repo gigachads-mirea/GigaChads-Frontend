5000a706e27f51229dbff7b348385bb7cc00a65b
Kirill <kirill.baikin@gmail.com>
2023-05-13 14:35:22 +0300

    Added RTK-Query and NextUI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import {NextUIProvider} from '@nextui-org/react';
+
+import {store} from '../app/store/store';
 
 import type {AppProps} from 'next/app';
 
-export default ({Component, pageProps}: AppProps) => <Component {...pageProps} />;
+export default ({Component, pageProps}: AppProps) => (
+	<NextUIProvider>
+		<Provider store={store}>
+			<Component {...pageProps} />
+		</Provider>
+	</NextUIProvider>
+);
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import { Html, Head, Main, NextScript } from 'next/document';
+// pages/_document.js
 
-export default () => <Html lang="en">
-	<Head/>
-	<body>
-		<Main />
-		<NextScript />
-	</body>
-</Html>;
+import React from 'react';
+import Document, {Head, Html, Main, NextScript} from 'next/document';
+import {CssBaseline} from '@nextui-org/react';
+
+class MyDocument extends Document {
+	static async getInitialProps(ctx) {
+		const initialProps = await Document.getInitialProps(ctx);
+		return {
+			...initialProps,
+			styles: React.Children.toArray([initialProps.styles]),
+		};
+	}
+
+	render() {
+		return (
+			<Html lang="en">
+				<Head>{CssBaseline.flush()}</Head>
+				<body>
+					<Main/>
+					<NextScript/>
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
